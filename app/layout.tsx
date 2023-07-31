9195b6280d3e19fc7efa8ceb32b8c12b9ac530f5
Philani Ncube <ncbphi001@gmail.com>
2023-07-31 15:35:53 +0200

    force dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import Footer from './Footer'
 import Navbar from './Navbar'
 import './globals.css'
 
+export const dynamic = "force-dynamic";
+
 
 export const metadata = {
   title: 'Hotwater 24',
