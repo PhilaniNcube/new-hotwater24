77ff41136bba76170baba047d333f532106392f4
Philani Ncube <ncbphi001@gmail.com>
2023-11-30 09:34:11 +0200

    Update gas geyser selection page layout and text

--- app/quote/start/page.tsx
+++ app/quote/start/page.tsx
@@ -6,52 +6,57 @@ import Link from "next/link";
 const page = () => {
   return (
     <main className="container py-10">
-      <div className="pt-4 w-full">
-        <h1 className={cn("text-2xl lg:text-4xl text-center font-semibold text-gray-800",antonio.className)}>
-          Let us help you choose your gas geyser!
+      <div className="w-full pt-4">
+        <h1
+          className={cn(
+            "text-2xl lg:text-4xl text-center font-semibold text-gray-800",
+            antonio.className
+          )}
+        >
+          Let us help you choose your gas geyser that is suitable for your home!
         </h1>
 
-        <p className="text-lg text-center px-8 lg:px-60">
-          Answer a few questions and we will instantly give you our expert
-          recommendation for a suitable geyser for your home
+        <p className="px-8 text-lg text-center lg:px-60">
+          Please answer the following questions (takes less than 2 minutes of
+          your time) and we will send you our expert recommendation!
         </p>
 
-        <div className="flex flex-col md:flex-row items-center py-8 justify-between">
-          <div className="flex flex-col w-64 h-64 shadow-lg rounded bg-gray-50 justify-center items-center hover:bg-gray-100 hover:shadow">
+        <div className="flex flex-col items-center justify-between py-8 md:flex-row">
+          <div className="flex flex-col items-center justify-center w-64 h-64 rounded shadow-lg bg-gray-50 hover:bg-gray-100 hover:shadow">
             <img
-              className="h-16 w-16"
+              className="w-16 h-16"
               alt=""
               src="/images/icons/clipboard.svg"
             />
-            <p className="text-sm py-8 text-center px-3">
+            <p className="px-3 py-8 text-sm text-center">
               Answer a few questions
             </p>
           </div>
-          <span className="rounded-full p-2 my-4 text-white bg-red-500 flex items-center justify-center">
+          <span className="flex items-center justify-center p-2 my-4 text-white bg-red-500 rounded-full">
             <ArrowRight className="rotate-90 lg:rotate-0" />
           </span>
 
-          <div className="flex flex-col w-64 h-64 shadow-lg rounded bg-gray-50 justify-center items-center hover:bg-gray-100 hover:shadow">
+          <div className="flex flex-col items-center justify-center w-64 h-64 rounded shadow-lg bg-gray-50 hover:bg-gray-100 hover:shadow">
             <img
-              className="h-16 w-16"
+              className="w-16 h-16"
               alt=""
               src="/images/icons/document.svg"
             />
-            <p className="text-sm py-8 text-center px-3">
+            <p className="px-3 py-8 text-sm text-center">
               We analyze your answers
             </p>
           </div>
 
-          <span className="rounded-full p-2 my-4 text-white bg-red-500 flex items-center justify-center">
+          <span className="flex items-center justify-center p-2 my-4 text-white bg-red-500 rounded-full">
             <ArrowRight className="rotate-90 lg:rotate-0" />
           </span>
-          <div className="flex flex-col w-64 h-64 shadow-lg rounded bg-gray-50 justify-center items-center hover:bg-gray-100 hover:shadow">
+          <div className="flex flex-col items-center justify-center w-64 h-64 rounded shadow-lg bg-gray-50 hover:bg-gray-100 hover:shadow">
             <img
-              className="h-16 w-16"
+              className="w-16 h-16"
               alt=""
               src="/images/icons/recommendation.svg"
             />
-            <p className="text-sm py-8 text-center px-3">Our Recommendation</p>
+            <p className="px-3 py-8 text-sm text-center">Our Recommendation</p>
           </div>
         </div>
 
@@ -59,7 +64,7 @@ const page = () => {
           <Link href="/quote" passHref>
             <button
               // onClick={start}
-              className="bg-sky-500 hover:bg-sky-700 text-center text-white text-2xl font-medium rounded-full py-3 px-8 shadow-sky-400 shadow-md hover:shadow"
+              className="px-8 py-3 text-2xl font-medium text-center text-white rounded-full shadow-md bg-sky-500 hover:bg-sky-700 shadow-sky-400 hover:shadow"
             >
               Let&#39;s get started!
             </button>
