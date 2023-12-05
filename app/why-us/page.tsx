5ccd93f18f3b006d63e873d8ea3bbaea3abc54e6
Philani Ncube <ncbphi001@gmail.com>
2023-12-05 08:58:08 +0200

    Remove unnecessary imports in why-us/page.tsx

--- app/why-us/page.tsx
+++ app/why-us/page.tsx
@@ -1,10 +1,7 @@
-import Choose from "@/components/Choose";
+
 import ChooseUsHero from "@/components/ChooseUs/choose-us-hero";
 import SafeInstallations from "@/components/ChooseUs/safe-installations";
 import ChooseLpg from "@/components/ChooseUs/why-choose-lpg";
-import { ArrowRight, FlagIcon, Users2Icon } from "lucide-react";
-import Link from "next/link";
-import { RiFilePaperLine } from "react-icons/ri";
 import WhyUsHero from "./_components/WhyUsHero";
 
 import type { Metadata } from "next";
