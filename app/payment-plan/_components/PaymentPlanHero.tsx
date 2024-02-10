6c6e8f444dbed713bd585b9c518fe740e8168e02
Philani Ncube <ncbphi001@gmail.com>
2024-02-10 14:37:04 +0200

    Refactor PaymentPlanHero component layout and styling

--- app/payment-plan/_components/PaymentPlanHero.tsx
+++ app/payment-plan/_components/PaymentPlanHero.tsx
@@ -9,62 +9,62 @@ import Image from "next/image";
  */
 export default function PaymentPlanHero() {
   return (
-    <section className="w-full grid grid-cols-1 md:grid-cols-2 relative isolate pb-8">
-      <Image
-        src="/images/banner-2.jpg"
-        width={960}
-        height={654}
-        alt="Banner"
-        className="w-full object-cover"
-      />
-      <div className="w-full container">
-        <div className="flex flex-col justify-center h-full items-center space-y-4 text-center">
-          <div className="space-y-2 py-10 md:py-4">
-            <h1
-              className={cn(
-                "text-3xl text-slate-800 font-bold  sm:text-4xl md:text-5xl flex flex-col gap-2",
-                antonio.className
-              )}
-            >
-              Payment Plan
-            </h1>
-            <p className="max-w-[900px] text-lg mt-4 text-slate-800">
-              At Hotwater24 we understand that we dont always have a budget
-              to pay an installation once. Therefore we offer a
-              payment plan that helps you paying for your installation.
-            </p>
-            <div className="flex justify-center items-center">
-              <Card className="p-4">
-                <CardContent className="">
-                  {/***List the steps in a payment plan */}
-                  <ol className="text-md gap-2 flex flex-col items-start list-decimal text-slate-700 font-medium ">
-                    <li>
-                      We only require a 50% deposit upon approval of the
-                      quotation{" "}
-                    </li>
-                    <li>
-                      After the installation is complete, the second payment of
-                      20% is due
-                    </li>
-                    <li>
-                      After 1 month of the installation date, the third payment
-                      of 15% is due
-                    </li>
-                    <li>
-                      After 2 months of the installation date, the final 15%
-                      payment is due
-                    </li>
-                    <li>
-                      After receiving the full payment, we will the process and
-                      issue the CoC.
-                    </li>
-                  </ol>
-                </CardContent>
-              </Card>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+			<section className="relative grid w-full grid-cols-1 pb-8 md:grid-cols-2 isolate">
+				<Image
+					src="/images/banner-2.jpg"
+					width={960}
+					height={654}
+					alt="Banner"
+					className="object-cover w-full"
+				/>
+				<div className="container w-full">
+					<div className="flex flex-col items-center justify-center h-full space-y-4 text-center">
+						<div className="py-10 space-y-2 md:py-4">
+							<h1
+								className={cn(
+									"text-3xl text-slate-800 font-bold  sm:text-4xl md:text-5xl flex flex-col gap-2",
+									antonio.className,
+								)}
+							>
+								Payment Plan
+							</h1>
+							<p className="max-w-[900px] text-lg mt-4 text-slate-800">
+								At Hotwater24 we understand that we dont always have a budget to
+								pay an installation once. Therefore we offer a payment plan that
+								helps you paying for your installation.
+							</p>
+							<div className="flex items-center justify-center">
+								<Card className="">
+									<CardContent className="px-3 py-4">
+										{/***List the steps in a payment plan */}
+										<ol className="flex flex-col items-start px-6 font-medium text-center list-decimal gap-y-2 text-md text-slate-700 ">
+											<li>
+												We only require a 50% deposit upon approval of the
+												quotation{" "}
+											</li>
+											<li>
+												After the installation is complete, the second payment
+												of 20% is due
+											</li>
+											<li>
+												After 1 month of the installation date, the third
+												payment of 15% is due
+											</li>
+											<li>
+												After 2 months of the installation date, the final 15%
+												payment is due
+											</li>
+											<li>
+												After receiving the full payment, we will the process
+												and issue the CoC.
+											</li>
+										</ol>
+									</CardContent>
+								</Card>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
 }
--- components/Homepage/why-choose-gas.tsx
+++ components/Homepage/why-choose-gas.tsx
@@ -11,78 +11,78 @@ import {  FlameIcon } from "lucide-react";
 
 export default function WhyGoGas() {
   return (
-    <section className="container">
-      <div className="px-6 py-12 bg-white lg:px-0 dark:bg-gray-800">
-        <div className="pb-10 text-center">
-          <h1
-            className={cn(
-              "text-4xl font-bold text-gray-800 dark:text-white",
-              antonio.className
-            )}
-          >
-            Why go gas?
-          </h1>
-          <p className="text-3xl font-bold text-gray-600">
-            Save up to 25% on your monthly electricity bill!
-          </p>
-        </div>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-          <Card>
-            <CardContent className="flex flex-col items-center p-6 space-y-4">
-              <FlameIcon
-                className="w-8 h-8 text-white bg-red-600 rounded-full p-1"
-                size={26}
-              />
-              <CardTitle>Instant Hot Water</CardTitle>
-              <CardDescription>
-                A gas geyser provides hot water on-demand without your family
-                ever having to experience a cold shower.
-              </CardDescription>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardContent className="flex flex-col items-center p-6 space-y-4">
-              <FlameIcon
-                className="w-8 h-8 text-white bg-red-600 rounded-full p-1"
-                size={26}
-              />
-              <CardTitle>Safe To Operate</CardTitle>
-              <CardDescription>
-                Gas geysers are safe and easy to operate, even during
-                loadshedding.
-              </CardDescription>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardContent className="flex flex-col items-center p-6 space-y-4">
-              <FlameIcon
-                className="w-8 h-8 text-white bg-red-600 rounded-full p-1"
-                size={26}
-              />
-              <CardTitle>Easy Maintenance</CardTitle>
-              <CardDescription>
-                The lifespan of a quality gas geyser can reach between 15-20
-                years and if properly maintained, there is little to no risk of
-                your geyser bursting like with an electric geyser.
-              </CardDescription>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardContent className="flex flex-col items-center p-6 space-y-4">
-              <FlameIcon
-                className="w-8 h-8 text-white bg-red-600 rounded-full p-1"
-                size={26}
-              />
-              <CardTitle>Eco-Friendly</CardTitle>
-              <CardDescription>
-                Modern day quality gas geysers are very efficient making them
-                more eco-friendly reducing your carbon footprint since water
-                will only be heated when needed.
-              </CardDescription>
-            </CardContent>
-          </Card>
-        </div>
-      </div>
-    </section>
-  );
+			<section className="container">
+				<div className="px-6 py-12 bg-white lg:px-0 dark:bg-gray-800">
+					<div className="pb-10 text-center">
+						<h1
+							className={cn(
+								"text-4xl font-bold text-gray-800 dark:text-white",
+								antonio.className,
+							)}
+						>
+							Why go gas?
+						</h1>
+						<p className="text-3xl font-bold text-gray-600">
+							Save up to 25% on your monthly electricity bill!
+						</p>
+					</div>
+					<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+						<Card>
+							<CardContent className="flex flex-col items-center p-6 space-y-4">
+								<FlameIcon
+									className="w-8 h-8 p-1 text-white bg-red-600 rounded-full"
+									size={26}
+								/>
+								<CardTitle>Instant Hot Water</CardTitle>
+								<CardDescription>
+									A gas geyser provides hot water on-demand without your family
+									ever having to experience a cold shower.
+								</CardDescription>
+							</CardContent>
+						</Card>
+						<Card>
+							<CardContent className="flex flex-col items-center p-6 space-y-4">
+								<FlameIcon
+									className="w-8 h-8 p-1 text-white bg-red-600 rounded-full"
+									size={26}
+								/>
+								<CardTitle>Safe To Operate</CardTitle>
+								<CardDescription>
+									Gas geysers are safe and easy to operate, even during
+									loadshedding.
+								</CardDescription>
+							</CardContent>
+						</Card>
+						<Card>
+							<CardContent className="flex flex-col items-center p-6 space-y-4">
+								<FlameIcon
+									className="w-8 h-8 p-1 text-white bg-red-600 rounded-full"
+									size={26}
+								/>
+								<CardTitle>Easy Maintenance</CardTitle>
+								<CardDescription>
+									The lifespan of a quality gas geyser can reach between 15-20
+									years and if properly maintained, there is little to no risk
+									of your geyser bursting like with an electric geyser.
+								</CardDescription>
+							</CardContent>
+						</Card>
+						<Card>
+							<CardContent className="flex flex-col items-center p-6 space-y-4">
+								<FlameIcon
+									className="w-8 h-8 p-1 text-white bg-red-600 rounded-full"
+									size={26}
+								/>
+								<CardTitle>Eco-Friendly</CardTitle>
+								<CardDescription>
+									Modern day quality gas geysers are very efficient making them
+									more eco-friendly and reducing your carbon footprint as water
+									will only be heated when needed.
+								</CardDescription>
+							</CardContent>
+						</Card>
+					</div>
+				</div>
+			</section>
+		);
 }
