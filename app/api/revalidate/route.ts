1b7a7829dd902e34d2a259201516a2b17f907050
Philani Ncube <ncbphi001@gmail.com>
2024-08-28 10:29:57 +0200

    Refactor revalidate route to revalidate news articles from Sanity webhook

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,22 @@
+
+import { createClient } from "@/utils/supabase/server";
+import { CookieOptions, createServerClient } from "@supabase/ssr";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req:Request) {
+
+  // revalidate all news articles from the Sanity webook
+  const body = await req.json()
+
+  console.log("Revalidating news articles", body)
+
+  revalidatePath('/news', "layout")
+
+	return NextResponse.json(
+		{ status: 200 },
+	);
+}
