01c1d78b7fd21d72ad8a1dfc95320653ea6d3671
Philani Ncube <ncbphi001@gmail.com>
2024-08-28 10:49:43 +0200

    Refactor revalidate route to revalidate news articles from Sanity webhook

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -9,10 +9,7 @@ export const dynamic = "force-dynamic";
 
 export async function POST(req:Request) {
 
-  // revalidate all news articles from the Sanity webook
-  const body = await req.json()
 
-  console.log("Revalidating news articles", body)
 
   revalidatePath('/news', "layout")
   revalidatePath('/', "layout")
