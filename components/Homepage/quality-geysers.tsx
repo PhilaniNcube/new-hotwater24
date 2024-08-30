7493de1c2205eb391ef7471b27837d6d9ce19a0c
Philani Ncube <ncbphi001@gmail.com>
2024-08-30 13:27:16 +0200

    Update quality-geysers component to display additional gas geyser options

--- components/Homepage/quality-geysers.tsx
+++ components/Homepage/quality-geysers.tsx
@@ -16,7 +16,7 @@ const QualityGeysers = () => {
 				>
 					Quality gas geysers
 				</h2>
-				<div className="grid grid-cols-1 gap-8 py-5 md:grid-cols-3">
+				<div className="grid grid-cols-1 gap-8 py-5 md:grid-cols-3 lg:grid-cols-5">
 					<article className="w-full">
 						<Card>
 							<CardHeader>
@@ -32,7 +32,7 @@ const QualityGeysers = () => {
 								<CardTitle className="sr-only">Dewhot</CardTitle>
 							</CardHeader>
 							<CardContent>
-								<CardDescription className="font-medium text-center text-md">
+								<CardDescription className="text-sm font-medium text-center">
 									With a commitment to quality and reliability, Dewhot&apos;s
 									double testing policy ensures a satisfying customer
 									experience.
@@ -55,7 +55,7 @@ const QualityGeysers = () => {
 								<CardTitle className="sr-only">Rinnai</CardTitle>
 							</CardHeader>
 							<CardContent>
-								<CardDescription className="font-medium text-center text-md">
+								<CardDescription className="text-sm font-medium text-center">
 									Rinnai is proud to be the No.1 choice for Continuous Flow Hot
 									Water, so you will never run out of hot water.
 								</CardDescription>
@@ -77,10 +77,56 @@ const QualityGeysers = () => {
 								<CardTitle className="sr-only">Paloma</CardTitle>
 							</CardHeader>
 							<CardContent>
-								<CardDescription className="font-medium text-center text-md">
+								<CardDescription className="text-sm font-medium text-center">
 									Paloma Gas Geysers is a trusted and well-established brand
 									offering top-quality gas geysers for residential and
 									commercial use.
+								</CardDescription>
+							</CardContent>
+						</Card>
+					</article>
+					<article>
+						<Card>
+							<CardHeader>
+								<Image
+									src="/images/bosch.webp"
+									width={1366}
+									height={768}
+									alt="Bosch"
+									className="object-cover w-1/3 mx-auto"
+									loading="lazy"
+									quality={70}
+								/>
+								<CardTitle className="sr-only">Bosch</CardTitle>
+							</CardHeader>
+							<CardContent>
+								<CardDescription className="text-sm font-medium text-center">
+									The Bosch gas geyser with battery or hydro ignition have all
+									the qualities expected of a Bosch appliance while at the same
+									time offering you the added benefit of being small and
+									efficient.
+								</CardDescription>
+							</CardContent>
+						</Card>
+					</article>
+					<article>
+						<Card>
+							<CardHeader>
+								<Image
+									src="/images/typhoon.webp"
+									width={762}
+									height={527}
+									alt="Typhoon Logo"
+									className="object-cover w-1/3 mx-auto"
+									loading="lazy"
+									quality={70}
+								/>
+								<CardTitle className="sr-only">Typhoon</CardTitle>
+							</CardHeader>
+							<CardContent>
+								<CardDescription className="text-sm font-medium text-center">
+									With a broad range in different size gas geysers Typhoon
+									offers the solution for every client’s requirement.
 								</CardDescription>
 							</CardContent>
 						</Card>
