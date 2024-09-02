c88e66b9b9ac6748bd02f5b78dcc8b8825a8ca49
Philani Ncube <ncbphi001@gmail.com>
2024-09-02 18:28:13 +0200

    feat: Add social media icons to the navigation bar

--- components/Navigation/Desktop.tsx
+++ components/Navigation/Desktop.tsx
@@ -17,8 +17,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
-import { RiWhatsappLine } from "react-icons/ri";
+import { RiFacebookBoxFill, RiInstagramFill, RiLinkedinFill, RiLinkedinLine, RiWhatsappLine } from "react-icons/ri";
 import AreasSelector from "./AreasSelector";
+import { AiFillRedEnvelope, AiOutlineRedEnvelope } from "react-icons/ai";
+import { Mail, Phone } from "lucide-react";
 
 type Props = {
   packages: Geyser[];
@@ -32,24 +34,69 @@ type Props = {
 
 const Desktop = ({ packages, cities }: Props) => {
   return (
-			<section className="sticky top-0 left-0 right-0 z-50 hidden py-3 shadow-md lg:block bg-white/80 backdrop-blur-sm">
-				<nav className="container flex items-center justify-between">
-					<div className="flex items-center gap-x-9">
-						<Link href="/" passHref>
-							<div className="flex items-center space-x-2 cursor-pointer">
-								<Image
-									className="object-cover w-36 sm:w-44"
-									src="/images/hw24-logo.svg"
-									width={358}
-									height={73}
-									alt="hotwater24"
-									aria-label="Logo"
-								/>
-							</div>
-						</Link>
-						<NavigationMenu>
-							<NavigationMenuList>
-								{/* <NavigationMenuItem>
+    <section className="sticky top-0 left-0 right-0 z-50 hidden py-3 shadow-md lg:block bg-white/80 backdrop-blur-sm">
+      <div className="container flex items-center justify-between py-2 mb-2 border-b md:flex-row">
+        <span className="flex flex-row items-center">
+          <Mail className="text-slate-600" size={20} />
+          <a
+            href="mailto:info@hotwater24.com"
+            className="ml-2 text-sm font-medium text-slate-600"
+          >
+            info@hotwater24.com
+          </a>
+          <span className="pl-3">|</span>
+          <Phone className="ml-2 text-slate-600" size={20} />
+          <a
+            href="tel:+27793414075"
+            className="ml-2 text-sm font-medium text-slate-600"
+          >
+            +27 79 341 4075
+          </a>
+        </span>
+        <span className="flex flex-row items-center">
+          <a
+            href="https://www.facebook.com/hotwater24"
+            target="_blank"
+            rel="noreferrer"
+            className="text-slate-600"
+          >
+            <RiFacebookBoxFill className="text-2xl" />
+          </a>
+          <a
+            href="https://www.instagram.com/hot_water_24/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-slate-600"
+          >
+            <RiInstagramFill className="text-2xl" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/hotwater24/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-slate-600"
+          >
+            <RiLinkedinFill className="text-2xl" />
+          </a>
+        </span>
+      </div>
+      <nav className="container flex items-center justify-between">
+        <div className="flex items-center gap-x-9">
+          <Link href="/" passHref>
+            <div className="flex items-center space-x-2 cursor-pointer">
+              <Image
+                className="object-cover w-36 sm:w-44"
+                src="/images/hw24-logo.svg"
+                width={358}
+                height={73}
+                alt="hotwater24"
+                aria-label="Logo"
+              />
+            </div>
+          </Link>
+          <NavigationMenu>
+            <NavigationMenuList>
+              {/* <NavigationMenuItem>
 									<NavigationMenuTrigger>Our Packages</NavigationMenuTrigger>
 									<NavigationMenuContent>
 										<ul className="grid gap-3 p-6 ">
@@ -74,79 +121,67 @@ const Desktop = ({ packages, cities }: Props) => {
 										</ul>
 									</NavigationMenuContent>
 								</NavigationMenuItem> */}
-								<NavigationMenuItem>
-									<Link href="/#packages" legacyBehavior passHref>
-										<NavigationMenuLink
-											className={navigationMenuTriggerStyle()}
-										>
-											Our Packages
-										</NavigationMenuLink>
-									</Link>
-								</NavigationMenuItem>
-								<NavigationMenuItem>
-									<Link href="/who-we-are" legacyBehavior passHref>
-										<NavigationMenuLink
-											className={navigationMenuTriggerStyle()}
-										>
-											Who We Are
-										</NavigationMenuLink>
-									</Link>
-								</NavigationMenuItem>
-								<NavigationMenuItem>
-									<Link href="/why-us" legacyBehavior passHref>
-										<NavigationMenuLink
-											className={navigationMenuTriggerStyle()}
-										>
-											Why Choose Us
-										</NavigationMenuLink>
-									</Link>
-								</NavigationMenuItem>
-								<NavigationMenuItem>
-									<Link href="/payment-plan" legacyBehavior passHref>
-										<NavigationMenuLink
-											className={navigationMenuTriggerStyle()}
-										>
-											Payment Plan
-										</NavigationMenuLink>
-									</Link>
-								</NavigationMenuItem>
-								<NavigationMenuItem>
-									<Link href="/faq" legacyBehavior passHref>
-										<NavigationMenuLink
-											className={navigationMenuTriggerStyle()}
-										>
-											FAQ's
-										</NavigationMenuLink>
-									</Link>
-								</NavigationMenuItem>
-								<NavigationMenuItem>
-									<Link href="/news" legacyBehavior passHref>
-										<NavigationMenuLink
-											className={navigationMenuTriggerStyle()}
-										>
-											News
-										</NavigationMenuLink>
-									</Link>
-								</NavigationMenuItem>
-							</NavigationMenuList>
-						</NavigationMenu>
-					</div>
-					<div className="flex items-center gap-4">
-						<AreasSelector cities={cities} />
-						<Link
-							href="https://wa.me/27793414075?text=I'm%20interested%20in%20your%20products"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<Button type="button" className="bg-green-500 rounded-full">
-								<RiWhatsappLine className="mr-1 text-2xl" />
-								Get In Touch
-							</Button>
-						</Link>
-					</div>
-				</nav>
-			</section>
-		);
+              <NavigationMenuItem>
+                <Link href="/#packages" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Our Packages
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/who-we-are" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Who We Are
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/why-us" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Why Choose Us
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/payment-plan" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Payment Plan
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/faq" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    FAQ's
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/news" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    News
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div className="flex items-center gap-4">
+          <AreasSelector cities={cities} />
+          <Link
+            href="https://wa.me/27793414075?text=I'm%20interested%20in%20your%20products"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button type="button" className="bg-green-500 rounded-full">
+              <RiWhatsappLine className="mr-1 text-2xl" />
+              Get In Touch
+            </Button>
+          </Link>
+        </div>
+      </nav>
+    </section>
+  );
 };
 export default Desktop;
 
