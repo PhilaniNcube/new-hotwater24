ec05e54bf1723a966c4837eea8c08d07b2843902
Philani Ncube <ncbphi001@gmail.com>
2024-09-24 21:54:49 +0200

    Refactor layout component to add AI Nav chatbot widget

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -47,17 +47,30 @@ export default async function Public({
 		.order("name", { ascending: true });
 
 	return (
-		<html lang="en">
-			<GoogleTagManager gtmId="GTM-WWK8FMB" />
-			<body className={lato.className}>
-				<GasGenius />
-				{/* biome-ignore lint/style/noNonNullAssertion: <explanation> */}
-				<Desktop packages={geysers} cities={data!} />
-				{/* biome-ignore lint/style/noNonNullAssertion: <explanation> */}
-				<Mobile packages={geysers} cities={data!} />
-				{children}
-				<Footer />
-			</body>
-		</html>
-	);
+    <html lang="en">
+      <GoogleTagManager gtmId="GTM-WWK8FMB" />
+      <body className={lato.className}>
+        <GasGenius />
+        {/* biome-ignore lint/style/noNonNullAssertion: <explanation> */}
+        <Desktop packages={geysers} cities={data!} />
+        {/* biome-ignore lint/style/noNonNullAssertion: <explanation> */}
+        <Mobile packages={geysers} cities={data!} />
+        {children}
+        <Footer />
+        {/* @ts-ignore */}
+        <co-pilot
+          platform-id="1b2a1f17-9d0c-45d6-be86-cf30ce9cf441"
+          user-id="60beee09-8302-48c1-809f-e3055b8a3b86"
+          chatbot-id="1a1e3fd8-f906-49a7-8362-8eab59886b9c"
+          is-local="false"
+        >
+          <a href="https://www.chatsimple.ai/?utm_source=widget&utm_medium=referral">
+            AI Nav
+          </a>
+          {/* @ts-ignore */}
+        </co-pilot>
+        <Script src="https://cdn.chatsimple.ai/ai-loader.js" defer></Script>
+      </body>
+    </html>
+  );
 }
