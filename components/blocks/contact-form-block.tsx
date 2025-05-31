f20bc400d741623e45a1404c2e775e22429646f6
Philani Ncube <ncbphi001@gmail.com>
2025-05-31 21:57:48 +0200

    Implement code changes to enhance functionality and improve performance

--- components/blocks/contact-form-block.tsx
+++ components/blocks/contact-form-block.tsx
@@ -3,6 +3,8 @@
 import { ContactFormSection } from "@/sanity/types";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
 
 interface ContactFormBlockProps {
   data: ContactFormSection;
@@ -39,72 +41,83 @@ export default function ContactFormBlock({ data }: ContactFormBlockProps) {
   };
 
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32">
-      <div className="container px-4 md:px-6">
-        <div className="max-w-2xl mx-auto">
-          <div className="flex flex-col items-center justify-center mb-8 space-y-4 text-center">
-            {heading && (
-              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-slate-800">
-                {heading}
-              </h2>
-            )}
-            {subheading && (
-              <p className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                {subheading}
+    <section className="w-full py-12">
+      <div className="container px-4 mx-auto max-w-7xl md:px-6">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          <div className="max-w-2xl mx-auto">
+            <div className="flex flex-col mb-8 space-y-4">
+              {heading && (
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-slate-800">
+                  {heading}
+                </h2>
+              )}
+              {subheading && (
+                <p className="text-gray-500 md:text-sm/relaxed lg:text-md/relaxed">
+                  {subheading}
+                </p>
+              )}
+            </div>
+
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                <div>
+                  <Input
+                    className="w-full p-4 text-base text-gray-600 border border-gray-300 rounded-md focus:outline-none focus:border-gray-700"
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    placeholder="Enter your Name"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <Input
+                    className="w-full p-4 text-base text-gray-600 border border-gray-300 rounded-md focus:outline-none focus:border-gray-700"
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    placeholder="Enter your email"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div>
+                <Textarea
+                  name="message"
+                  value={formData.message}
+                  onChange={handleInputChange}
+                  placeholder="Your message"
+                  rows={4}
+                  className="w-full p-4 text-base text-gray-600 border border-gray-300 rounded-md resize-none focus:outline-none focus:border-gray-700"
+                  required
+                />
+              </div>
+
+              <Button
+                type="submit"
+                disabled={isSubmitting}
+                className="w-full p-4 text-base font-medium text-white bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600"
+              >
+                {isSubmitting ? "Sending..." : submitButtonText}
+              </Button>
+
+              <p className="text-xs text-center text-gray-500">
+                By filling in this form you give us consent to email you – but
+                you can unsubscribe at any time
               </p>
-            )}
+            </form>
           </div>
-
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div>
-              <input
-                className="w-full p-4 text-base text-gray-600 border border-gray-300 rounded-md focus:outline-none focus:border-gray-700"
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleInputChange}
-                placeholder="Enter your Name"
-                required
-              />
-            </div>
-
-            <div>
-              <input
-                className="w-full p-4 text-base text-gray-600 border border-gray-300 rounded-md focus:outline-none focus:border-gray-700"
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-                placeholder="Enter your email"
-                required
-              />
-            </div>
-
-            <div>
-              <textarea
-                name="message"
-                value={formData.message}
-                onChange={handleInputChange}
-                placeholder="Your message"
-                rows={6}
-                className="w-full p-4 text-base text-gray-600 border border-gray-300 rounded-md resize-none focus:outline-none focus:border-gray-700"
-                required
-              />
-            </div>
-
-            <Button
-              type="submit"
-              disabled={isSubmitting}
-              className="w-full p-4 text-base font-medium text-white bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600"
-            >
-              {isSubmitting ? "Sending..." : submitButtonText}
-            </Button>
-
-            <p className="text-xs text-center text-gray-500">
-              By filling in this form you give us consent to email you – but you
-              can unsubscribe at any time
-            </p>
-          </form>
+          <div className="hidden lg:flex lg:items-center lg:justify-center">
+            <img
+              src="/images/installer.png"
+              alt="Contact Us"
+              className="w-2/3 h-auto mx-auto rounded-t-full rounded-b-full"
+            />
+          </div>
         </div>
       </div>
     </section>
