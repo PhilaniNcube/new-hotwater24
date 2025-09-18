5ede40916c01b6d4c5b4735f62f3913c46f5c2a2
Philani Ncube <ncbphi001@gmail.com>
2025-09-18 09:03:05 +0200

    refactor: simplify page component structure and improve readability

--- app/(public)/areas/[city]/page.tsx
+++ app/(public)/areas/[city]/page.tsx
@@ -1,29 +1,23 @@
-
 import CityHero from "./_components/CityHero";
 import CitySection from "./_components/CitySection";
 import { areas } from "@/utils/areas";
 
 export const dynamic = "force-static";
 
+const page = async ({ params }: { params: Promise<{ city: string }> }) => {
+  const { city } = await params;
 
-const page = async ({params}:{params:Promise<{city:string}>}) => {
+  const area = areas.find((area) => area.slug === city);
 
-  const { city } = await params;     
-  
-      const area = areas.find((area) => area.slug === city);
-
-      if(!area) {
-        return {
-          notFound: true
-        }
-
-      }
+  if (!area) {
+    return <div className="container p-4 mx-auto">City not found</div>;
+  }
 
   return (
-			<main>
-				<CityHero city={area?.name} />
-				<CitySection city={area?.name} />
-			</main>
-		);
+    <main>
+      <CityHero city={area?.name} />
+      <CitySection city={area?.name} />
+    </main>
+  );
 };
 export default page;
